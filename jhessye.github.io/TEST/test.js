c5539faa65607aa3d3199bc09b3c22e0709313c2
SolidToco <134889567+SolidToco@users.noreply.github.com>
2023-07-07 23:56:41 -0300

    Delete jhessye.github.io directory

--- jhessye.github.io/TEST/test.js
+++
@@ -1,7 +0,0 @@
-function onSubmit() {
-    if (document.getElementById('pass').value == '1234') {
-      window.location.href = "https://www.w3schools.com";
-    } else {
-      alert('Access Denied, Please try again');
-    }
-  }
